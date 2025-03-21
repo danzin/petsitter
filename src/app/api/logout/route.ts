f02038221f03f341bfcb78048c6dc05c8a66025a
danzin <excl.d.k@gmail.com>
2025-03-21 20:49:41 +0200

    experimenting with server actions

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -4,9 +4,7 @@ import { createAuthOptions } from "@/lib/auth/nextAuthConfig";
 
 export async function GET() {
   const session = await getServerSession(createAuthOptions());
-  const response = NextResponse.redirect(
-    new URL("/login", "http://localhost:3000")
-  );
+  const response = NextResponse.redirect(new URL("/", "http://localhost:3000"));
 
   if (session) {
     response.cookies.delete("next-auth.session-token");
